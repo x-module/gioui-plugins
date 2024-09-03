1fb0ed84db67c11f87d7209f8821481102e55916
x-module <244395692@qq.com>
2024-09-03 20:17:58 +0800

    init

--- test/tooltip.go
+++ test/tooltip.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"gioui.org/app"
 	"gioui.org/layout"
 	"gioui.org/op"
 	"gioui.org/unit"
 	"gioui.org/widget"
+	"gioui.org/x/component"
 	"github.com/x-module/gioui-plugins/resource"
 	"github.com/x-module/gioui-plugins/theme"
 	"github.com/x-module/gioui-plugins/widgets"
@@ -14,27 +16,33 @@ import (
 
 func main() {
 	var clickable widget.Clickable
+
 	var th = theme.NewTheme()
 	card := widgets.NewCard(th)
-	win := window.NewApplication(new(app.Window))
+	win := window.NewApplication(new(app.Window)).CenterWindow()
 	win.Title("Hello, Gio!").Size(window.ElementSize{
 		Height: 600,
 		Width:  800,
 	})
+	var content widget.Bool
 	win.BackgroundColor(th.Color.DefaultWindowBgGrayColor)
-	win.NoActionBar().CenterWindow()
-	win.Frame(func(gtx layout.Context, ops op.Ops, w *app.Window) {
+	win.Frame(func(gtx layout.Context, ops op.Ops, win *app.Window) {
 		if clickable.Clicked(gtx) {
-			win.Size(window.ElementSize{
-				Width:  1200,
-				Height: 900,
-			}).ReCenterWindow()
+			fmt.Println("clicked")
 		}
 		layout.UniformInset(unit.Dp(20)).Layout(gtx, func(gtx layout.Context) layout.Dimensions {
 			return layout.Flex{Axis: layout.Vertical}.Layout(gtx,
 				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-					return card.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-						return widgets.DefaultButton(th, &clickable, "default", unit.Dp(100)).SetIcon(resource.DeleteIcon, 0).Layout(gtx)
+					return content.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+						dims := widgets.Label(th, "hello").Layout(gtx)
+
+						if content.Hovered() {
+							fmt.Println("hovered")
+							layout.Inset{Top: unit.Dp(3)}.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+								return component.PlatformTooltip(th.Material(), "hello").Layout(gtx)
+							})
+						}
+						return dims
 					})
 				}),
 				layout.Rigid(layout.Spacer{Height: unit.Dp(10)}.Layout),
